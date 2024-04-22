b08fd2fdba8570d6ea2133b017f61cd8623c9466
tensymb0ls <tensym8ols@gmail.com>
2024-04-22 20:14:26 +0300

    test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration.
 const firebaseConfig = {
   apiKey: "AIzaSyDiHpW9lpaVXdGjNeUOCHbMjA_9yrL1xgM",
   authDomain: "todo-d0db3.firebaseapp.com",
